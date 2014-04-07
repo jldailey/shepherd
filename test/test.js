832dcf78c2b0173f090d3a7992ee0fc03643d949
Jesse Dailey <jesse.dailey@gmail.com>
2014-04-07 07:08:51 -0400

    WIP on testing

--- test/test.js
+++ test/test.js
@@ -1,18 +1,40 @@
 
+$ = require("bling")
 Shell = require("shelljs")
 
 describe("shepherd", function() {
 
-	beforeEach(function() {
-		Shell.exec("mv fixtures/repo/_git fixtures/repo/.git", function() {
-			Shell.exec("mv fixtures/server/_git fixtures/server/.git", function() {
+	beforeEach(function(done) {
+		Shell.exec("mv test/fixtures/repo/_git test/fixtures/repo/.git", function(exitCode) {
+			Shell.exec("mv test/fixtures/server/_git test/fixtures/server/.git", function(exitCode) {
+				done();
 			})
 		})
 	})
 
-	afterEach(function() {
-		Shell.exec("mv fixtures/repo/.git fixtures/repo/_git", function() {
-			Shell.exec("mv fixtures/server/.git fixtures/server/_git", function() {
+	it("runs in a cwd", function(done) {
+		Shell.exec("ls -l", { silent: true, async: true }, function(exitCode) {
+			done();
+		})
+	})
+
+	it("starts children", function(done) {
+		shepherd = Shell.exec("bin/shepherd -f test/.herd", { silent: true, async: true }, function(exitCode) {
+			$.log("shepherd exitCode:", exitCode)
+			done();
+		})
+		shepherd.stdout.on('data', function(data) {
+			$.log(data)
+		})
+		shepherd.stderr.on('data', function(data) {
+			$.log("stderr", data)
+		})
+	})
+
+	afterEach(function(done) {
+		Shell.exec("mv test/fixtures/repo/.git test/fixtures/repo/_git", function(exitCode) {
+			Shell.exec("mv test/fixtures/server/.git test/fixtures/server/_git", function(exitCode) {
+				done();
 			})
 		})
 	})
