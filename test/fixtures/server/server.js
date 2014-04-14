3884438fb7457b55c0ec1abcb2d7d8bd285197ba
Jesse Dailey <jesse.dailey@gmail.com>
2014-04-14 16:18:15 -0400

    modify the unit-test server to log requests

--- test/fixtures/server/server.js
+++ test/fixtures/server/server.js
@@ -5,4 +5,5 @@ console.log("Listening on port", process.env.PORT)
 Http.createServer(function(req, res) {
 	res.statusCode = 200;
 	res.end("OK: " + String(process.env.PORT));
+	console.log(req.method + " " + req.path + " " + res.statusCode)
 }).listen(process.env.PORT);
