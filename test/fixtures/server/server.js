40516cfd7b9faf09dc27de2c34f45cda35dcf07b
Jesse Dailey <jesse.dailey@gmail.com>
2014-04-14 16:38:33 -0400

    modify test server to have artificial startup delay

--- test/fixtures/server/server.js
+++ test/fixtures/server/server.js
@@ -1,9 +1,10 @@
 Http = require('http')
 
-console.log("Listening on port", process.env.PORT)
-
-Http.createServer(function(req, res) {
-	res.statusCode = 200;
-	res.end("OK: " + String(process.env.PORT));
-	console.log(req.method + " " + req.path + " " + res.statusCode)
-}).listen(process.env.PORT);
+setTimeout(function() {
+	Http.createServer(function(req, res) {
+		res.statusCode = 200;
+		res.end("OK: " + String(process.env.PORT));
+		console.log(req.method + " " + req.url + " " + res.statusCode)
+	}).listen(process.env.PORT);
+	console.log("Listening on port", process.env.PORT)
+}, 2000) // add an artificial startup delay
