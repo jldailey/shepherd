dfeb5d27517bc50686473f4190f9251147bff100
Jesse Dailey <jesse.dailey@gmail.com>
2014-10-03 15:25:05 -0700

    first complete working test!

--- test/common.sh
+++ test/common.sh
@@ -1,13 +1,20 @@
 
 function kill_owner {
 	local port=$1
-	echo -n PORT: ${port}
 	local pid=`lsof -i :${port} | grep :${port} | awk '{print $2}'`
-	echo -n " PID:" ${pid}
 	if [ -n "${pid}" ]; then
-		echo "Killing..."
+		echo "Killing $PID (to clear port $PORT)"
 		kill -9 ${pid}
+	fi
+}
+
+function check_output {
+	local port=$1
+	local expected=$2
+	local output=`curl -s http://localhost:$port/`
+	if [ "$output" != "$expected" ]; then
+		echo "Unexpected output: '" $output "' expected: '" $expected "'"
 	else
-		echo
+		echo "PASS"
 	fi
 }
--- test/simple-startup.sh
+++ test/simple-startup.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-source `dirname $0`/common.sh
+ROOT=`dirname $0`/..
+source "$ROOT/test/common.sh"
 
 JSON_FILE=/tmp/simple-startup.json
+PID_FILE=/tmp/simple-startup.pid
+LOG_FILE=/tmp/simple-startup.log
+PORTS="8001 8002 8003"
 
 echo '
 {
 	"servers": [ {
-		"count": 2,
+		"count": 3,
 		"port": 8001,
 		"cd": "test/server",
 		"command": "node app.js",
@@ -19,11 +23,19 @@ echo '
 }
 ' > $JSON_FILE
 
+echo > $LOG_FILE
 # this test is not concerned with starting 'over the top'
-# of an already running instance, so we kill it all
+# of an already running instance, so we kill it all first
 kill_owner 9001
-kill_owner 8001
-kill_owner 8002
+for PORT in $PORTS; do
+	kill_owner $PORT
+done
 
-./bin/shepherd -v -f $JSON_FILE
-rm $JSON_FILE
+$ROOT/bin/shepherd -v -o $LOG_FILE -f $JSON_FILE -d -p $PID_FILE
+sleep 4
+PID=`cat $PID_FILE`
+for PORT in $PORTS; do
+	check_output $PORT "{\"PORT\": $PORT, \"TOKEN\": \"123456\"}" 
+done
+kill -2 $PID || echo "Failed to kill pid:" $PID
+rm -f $PID_FILE $JSON_FILE $LOG_FILE
