52c15d0b7ed3f6a9db6f1be4949fe51c9ad67738
Jesse Dailey <jesse.dailey@gmail.com>
2014-10-05 14:01:57 -0400

    add shepherd_start and shepherd_stop functions in common; use them in tests

--- test/common.sh
+++ test/common.sh
@@ -1,7 +1,25 @@
 
+JSON_FILE=/tmp/${TEST_NAME}.json
+PID_FILE=/tmp/${TEST_NAME}.pid
+LOG_FILE=/tmp/${TEST_NAME}.log
+
+function shepherd_start {
+	echo Launching shepherd...
+	$ROOT/bin/shepherd -v -o $LOG_FILE -f $JSON_FILE -d -p $PID_FILE
+	sleep 4
+	return `cat $PID_FILE`
+}
+
+function shepherd_stop {
+	echo "Stopping shepherd..."
+	PID=`cat $PID_FILE`
+	kill -2 $PID || echo "Failed to kill shepherd pid:" $PID
+	rm -f $PID_FILE $JSON_FILE $LOG_FILE
+}
+
 function kill_owner {
 	local port=$1
-	local pid=`lsof -i :${port} | grep :${port} | awk '{print $2}'`
+	local pid=`lsof -Pni :${port} | grep :${port} | awk '{print $2}'`
 	if [ -n "${pid}" ]; then
 		echo "Killing $PID (to clear port $PORT)"
 		kill -9 ${pid}
@@ -14,6 +32,8 @@ function check_output {
 	local output=`curl -s http://localhost:$port/`
 	if [ "$output" != "$expected" ]; then
 		echo "Unexpected output: '" $output "' expected: '" $expected "'"
+		shepherd_stop
+		exit 1
 	else
 		echo "PASS"
 	fi
--- test/simple-startup.sh
+++ test/simple-startup.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 ROOT=`dirname $0`/..
-source "$ROOT/test/common.sh"
+TEST_NAME=`basename $0 | sed s/\.sh//`
+source $ROOT/test/common.sh
 
-JSON_FILE=/tmp/simple-startup.json
-PID_FILE=/tmp/simple-startup.pid
-LOG_FILE=/tmp/simple-startup.log
 PORTS="8001 8002 8003"
 
 echo '
@@ -14,8 +12,7 @@ echo '
 		"count": 3,
 		"port": 8001,
 		"cd": "test/server",
-		"command": "node app.js",
-		"poolName": "shepherd_pool"
+		"command": "node app.js"
 	} ],
 	"admin": { "port": 9001 },
 	"nginx": { "enabled": false },
@@ -31,11 +28,8 @@ for PORT in $PORTS; do
 	kill_owner $PORT
 done
 
-$ROOT/bin/shepherd -v -o $LOG_FILE -f $JSON_FILE -d -p $PID_FILE
-sleep 4
-PID=`cat $PID_FILE`
+shepherd_start
 for PORT in $PORTS; do
 	check_output $PORT "{\"PORT\": $PORT, \"TOKEN\": \"123456\"}" 
 done
-kill -2 $PID || echo "Failed to kill pid:" $PID
-rm -f $PID_FILE $JSON_FILE $LOG_FILE
+shepherd_stop
