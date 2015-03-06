32d8f5bd81dc3f322dfd199d18db6d1875dfd470
Jesse Dailey <jesse.dailey@gmail.com>
2015-03-06 16:31:52 -0500

    test: add a test for when the server crashes very quickly on the first start attempt

---
+++ test/10-fast-crash.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+ROOT=`dirname $0`/..
+source $ROOT/test/common.sh
+
+echo '
+{
+	"servers": [ {
+		"count": 1,
+		"port": 9002,
+		"cd": "test/server",
+		"command": "node app.js crash-mode"
+	}, {
+		"count": 2,
+		"port": 9003,
+		"cd": "test/server",
+		"command": "node app.js"
+	} ],
+	"admin": { "port": 9001 },
+	"nginx": { "enabled": false },
+	"rabbitmq": { "enabled": false }
+}
+' > $JSON_FILE
+PORTS="9002 9003 9004"
+
+# this test is not concerned with starting 'over the top'
+# of an already running instance, so we kill it all first
+kill_owner 9001
+for PORT in $PORTS; do
+	kill_owner $PORT
+done
+
+shepherd_start
+for PORT in $PORTS; do
+	check_output $PORT
+done
+shepherd_stop
