e12909a7a78803d8cffa26a1d354bf207a5d3c6c
Jesse Dailey <jesse.dailey@gmail.com>
2016-03-25 15:55:14 -0400

    work in progress; the skeleton of an idea is working

--- src/test.sh
+++ src/test.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
-coffee src/daemon.coffee stop && coffee src/daemon.coffee start &
+SHEP="coffee src/client/index.coffee"
+SHEPD="coffee src/daemon/index.coffee"
+
+$SHEPD stop && \
+	$SHEPD start &
 sleep 1
-coffee src/opts.coffee add --group api --cd test/server --exec 'node app.js' -n 4 --port 8000
-coffee src/opts.coffee start --instance api-1
+$SHEP add --group api --cd test/server --exec 'node app.js' -n 4 --port 8000
+$SHEP start --instance api-1
 sleep 1
-coffee src/opts.coffee status
+$SHEP status
 
